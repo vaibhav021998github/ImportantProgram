3dab60a8c0e82b4f42d0b3520005cee3d4e3fc26
Vaibhav <vaibhav021998@gmail.com>
2023-06-17 21:56:38 +0530

    "First Commit"

--- Interview Important Codes/src/com/test/ReverseString.java
+++ Interview Important Codes/src/com/test/ReverseString.java
@@ -3,14 +3,6 @@ package com.test;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// Reverse String Program
-		String s="ram";
-		String s1="";
-		for(int i=s.length()-1;i>=0;i--) {
-			s1+=s.charAt(i);
-		}
-		System.out.println("Reverse String : "+s1);
-
 	}
 
 }
